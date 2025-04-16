dd1b934ae2e12bfb0157273d5b36f5fc15771d65
richie <richiepagard@gmail.com>
2025-04-16 12:44:54 +0330

    feat: Implement the Brute Force attack for Ceasar Cipher decryption.
    - Implement a Brute Force attack function for test 52 steps on a decrypt function to find the ecrypted data.
    
    - Clean doc-strings and comments.

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,14 +1,21 @@
-""" This is a cipher algorithm
-    :function find_index: It's can use instead of `.index` method in Python strings.
-    :function caesar_cipher_encrypt: Encrypting by this function.
-    :function caesar_cipher_decrypt: Dycrypting by this function.
+""" 
+A kind of cipher algorithm.
+    
+    Functions:
+        find_index: find the index of a given `char` in the given `string`.
+        encrypt: Encrypting the given data with a key provided by the function.
+        decrypt: Decrypting the given data with reversing the key provied by the function.
+        brute_force: A function for the Brute Force attack by testing 52 steps from 1-52.
 """
 
 from string import ascii_letters
 
 
+ALPHA = ascii_letters
+
 def find_index(string: str, char: str) -> int:
-    """I don't want to use `.index` method in Python strings, 
+    """
+    I don't want to use `.index` method in Python strings, 
     then I wrote this to underestand how `.index.` works.
     """
 
@@ -17,32 +24,48 @@ def find_index(string: str, char: str) -> int:
             return ch
     return "Your index not found in this string."
 
-def caesar_cipher_encrypt(input_string: str, key: int) -> int:
-    """The encrypting function
-        :param input_string: The string that you want to be encrypted.
-        :param key: The amount by wich you want to shift to the next letter.
-        :variable alpha: This variable has all alpha letters.
-        :variable result: The result of encrypting this string.
+def encrypt(data: str, key: int) -> int:
     """
+    The encrypting function.
 
-    alpha = ascii_letters
+    Attributes:
+        data(str): The string will be encrypted.
+        key(int): The step(shift).
+    """
     result = ''
 
-    for char in input_string:
-        if char not in alpha:
+    for char in data:
+        if char not in ALPHA:
             result += char
         else:
-            alpha_key = (find_index(alpha, char) + key) % len(alpha)
-            # you can use `alpha.index(char)` instead of `find_index(alpha, char)` to use Python abilities.
-            result += alpha[alpha_key]
+            alpha_key = (find_index(ALPHA, char) + key) % len(ALPHA)
+            # you can use `alpha.index(char)` instead of `find_index(ALPHA, char)` to use Python abilities.
+            result += ALPHA[alpha_key]
     return result
 
 
-def caesar_cipher_decrypt(input_string: str, key: int) -> str:
-    """The decrypting function
-        :param input_string: The string that you want to be encrypted.
-        :param key: The amount by wich you want to shift to the next letter.
+def decrypt(data: str, key: int) -> str:
+    """
+    The decrypting function.
+
+    Attributes:
+        data(str): The encrypted string that will be decrypted.
+        key(str): The step(shift).
     """
 
-    key *= -1   # negate the key for reversing the encrypted word
-    return caesar_cipher_encrypt(input_string=input_string, key=key)    # return encrypting function with negative key
+    # Reverse the key to decrypt the data
+    key *= -1
+    # Return decrypted function with a new key(reversed key)
+    return encrypt(data=data, key=key)
+
+def brute_force(data):
+    """
+    The brute force function for find the decrypted string from 1-52 steps.
+    """
+    brute_force_data = {}
+
+    for step in range(1, len(ALPHA)+1):
+        # Call decrypt function for test 52 steps on it
+        brute_force_data[step] = decrypt(data, step)
+
+    return brute_force_data
